609c912b128448f416097a1cd8b7651cfc41454f
shinya <ppeter1015@gmail.com>
2025-04-30 07:12:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const songStreamLinks = {
     { date: "2025年4月27日", videoId: "cHrzKWf3mcw", timestamp: 6976 },
     ],
   "Magical Babyrinth/DA PUMP": [
-     { date: "2025年4月16日(アカペラ)", videoId: "7A2DbCtnoMc", timestamp: 5760 },
+      { date: "2025年4月29日", videoId: "VZ8-0GdE7Ho", timestamp: 7250},
+      { date: "2025年4月16日(アカペラ)", videoId: "7A2DbCtnoMc", timestamp: 5760 },
   ],
   "ハヤテのごとく!/KOTOKO": [
     { date: "2025年4月13日", videoId: "f50svs6t8pU", timestamp: 13612 },
