a58ff570033cfc0da7ca46283224c2daf4905525
shinya <ppeter1015@gmail.com>
2025-04-30 08:30:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -145,8 +145,9 @@ const songStreamLinks = {
      { date: "2024年12月10日(アカペラ)", videoId: "WULCzP9OiTw", timestamp: 8794 },
   ],
   "Wonderful Wonder World*/Yun*chi": [
-     { date: "2025年4月7日", videoId: "7h27-6vwmrk", timestamp: 1042 },
-     { date: "2025年4月1日", videoId: "wNMS4XNQuh4", timestamp: 2030 },
+      { date: "2025年4月29日", videoId: "VZ8-0GdE7Ho", timestamp: 12768},
+      { date: "2025年4月7日", videoId: "7h27-6vwmrk", timestamp: 1042 },
+      { date: "2025年4月1日", videoId: "wNMS4XNQuh4", timestamp: 2030 },
   ],
   "アイコトバ/サクラメリーメン": [
      { date: "2025年4月1日", videoId: "wNMS4XNQuh4", timestamp: 1349 },
