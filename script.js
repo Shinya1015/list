93e68453820eb580a668abf377d1abb6f9bb9794
shinya <ppeter1015@gmail.com>
2025-05-11 08:09:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4959,7 +4959,8 @@ const songStreamLinks = {
       { date: "2025年4月13日", videoId: "f50svs6t8pU", timestamp: 4741},
       { date: "2024年9月10日", videoId: "9PoJ2QbvWmA", timestamp: 2985},
   ],
-  "nexus/ClariS": [
+   "nexus/ClariS": [
+      { date: "2025年5月10日", videoId: "Fxvnbk3viFA", timestamp: 6087},
       { date: "2025年4月13日", videoId: "f50svs6t8pU", timestamp: 16290},
       { date: "2025年4月7日", videoId: "7h27-6vwmrk", timestamp: 5325},
       { date: "2025年4月1日", videoId: "wNMS4XNQuh4", timestamp: 3226},
