601f1a0895d65b3c44b30ccbccab7a5feab390b2
shinya <ppeter1015@gmail.com>
2025-05-12 01:16:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -166,6 +166,7 @@ const songStreamLinks = {
      { date: "2025年4月1日", videoId: "wNMS4XNQuh4", timestamp: 17118 }, 
   ],
   "果てなき道/HIMEKA": [
+     { date: "2025年5月11日", videoId: "VgCnnKZ-XUc", timestamp: 3449},
      { date: "2025年5月6日", videoId: "5heXo7iMt0I", timestamp: 7789},     
      { date: "2025年4月29日", videoId: "VZ8-0GdE7Ho", timestamp: 3135},
      { date: "2025年4月13日", videoId: "f50svs6t8pU", timestamp: 3191 },
