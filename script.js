f89e3de8144ec12a2b6eb5f016385e3c8b347844
shinya <ppeter1015@gmail.com>
2025-05-14 09:15:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1432,7 +1432,8 @@ const songStreamLinks = {
     { },
   ],
   "ふわふわ♪/牧野由依": [
-    { },
+     { date: "2023年10月13日", videoId: "YwhvzxTlELM", timestamp: 8394},
+     { date: "2023年1月18日", videoId: "ME1ONTBCpho", timestamp: 13335},
   ],
   "おもかげ/林原めぐみ": [
     { },
