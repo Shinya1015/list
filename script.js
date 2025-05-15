ac542c76f742a0d6558d16c0f7f27ef999e81c60
shinya <ppeter1015@gmail.com>
2025-05-16 01:30:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -882,6 +882,7 @@ const songStreamLinks = {
   ],
   "素晴らしき世界/Rake": [
      { date: "2025年5月16日", videoId: "uslrbIZ4sDw", timestamp: 4040},
+     { date: "2025年4月10日", videoId: "QUsI2G0TNdQ", timestamp: 10527},
   ],
   "Love so sweet/嵐": [
     { },
