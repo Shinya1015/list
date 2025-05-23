7a59579a7beb3b0cc52ab2bfa74a71318adeeeb3
shinya <ppeter1015@gmail.com>
2025-05-23 08:30:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4172,6 +4172,7 @@ const songStreamLinks = {
       { date: "2023年6月28日", videoId: "rC7TsMmO3rA", timestamp: 6868},
   ],
   "アンビバレント/Uru ": [
+     { date: "2025年5月23日", videoId: "EBzrew6Ym1U", timestamp: 2736},
      { date: "2025年5月19日", videoId: "t9f_8P7y02c", timestamp: 2330},
      { date: "2025年5月6日", videoId: "5heXo7iMt0I", timestamp: 13031},
      { date: "2025年4月29日", videoId: "VZ8-0GdE7Ho", timestamp: 9777},
