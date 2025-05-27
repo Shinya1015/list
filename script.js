4c41641cc41d8fbccba5ab3ee20e55d20a4bbd41
shinya <ppeter1015@gmail.com>
2025-05-27 09:48:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -603,6 +603,7 @@ const songStreamLinks = {
      { date: "2023年1月2日", videoId: "VRx4XsQ4PeE", timestamp: 10763},
   ],
  "ベテルギウス/優里": [
+    { date: "2025年5月26日", videoId: "qWOH4XiPvfY", timestamp: 9762},
     { date: "2025年5月13日", videoId: "zPR8pqQgpCE", timestamp: 566},
     { date: "2025年5月10日", videoId: "Fxvnbk3viFA", timestamp: 5399},
     { date: "2025年3月18日-piano ver.", videoId: "oXcvvDAPpEU", timestamp: 7394 },
