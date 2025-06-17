7f0f57140ce55eb0782ae206a6d5c632a72b02ac
shinya <ppeter1015@gmail.com>
2025-06-18 00:45:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1703,6 +1703,7 @@ const songStreamLinks = {
     { date: "2023年1月5日", videoId: "Hd9_63cEDp4", timestamp: 14406 },
   ],
   "Time after time～花舞う街で～/倉木麻衣": [
+     { date: "2025年6月17日", videoId: "dOchrd8Pxwo", timestamp: 6712},
      { date: "2025年5月23日", videoId: "EBzrew6Ym1U", timestamp: 1981},
      { date: "2025年5月10日", videoId: "Fxvnbk3viFA", timestamp: 7724},
      { date: "2025年5月6日", videoId: "5heXo7iMt0I", timestamp: 10258},
