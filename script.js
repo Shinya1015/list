5c00883ed714de97691d7b81d6128b192b39d839
shinya <ppeter1015@gmail.com>
2025-07-27 22:29:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8604,7 +8604,8 @@ const songStreamLinks = {
     { date: "2023年8月20日", videoId: "qeFbHHe1zKE", timestamp: 7086 },
     { date: "2023年8月16日", videoId: "3O4U8gGiR1Y", timestamp: 2374 },
   ],
-  "天体観測/BUMP OF CHICKEN": [
+　"天体観測/BUMP OF CHICKEN": [
+    { date: "2025年7月27日", videoId: "syJnL38D7-Q", timestamp: 8211},
     { date: "2024年11月24日", videoId: "YqzVwANwxZ4", timestamp: 3395 },
     { date: "2024年11月16日", videoId: "y3fuUYZ_mOA", timestamp: 2309 },
     { date: "2024年10月9日", videoId: "jM1806vMFtU", timestamp: 8182 },
