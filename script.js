8f299836c8afe3d975aef5b042d6ea100565c48d
shinya <ppeter1015@gmail.com>
2026-09-23 07:38:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9127,7 +9127,7 @@ const songStreamLinks = {
      { date: "2024年4月23日", videoId: "RsW_0ey6VL0", timestamp: 3977},
      { date: "2024年1月17日", videoId: "uq6bkRYAZvI", timestamp: 12467},
      { date: "2023年6月12日", videoId: "P7vvZTtvB4g", timestamp: 7522},
-     { date: "2023年5月25日", videoId: "0D7De4cmwf0", timestamp: 3496},+
+     { date: "2023年5月25日", videoId: "0D7De4cmwf0", timestamp: 3496},
      { date: "2023年2月13日", videoId: "WDaTU01Humc", timestamp: 11563},
      { date: "2023年1月16日", videoId: "czmPQXUWDsA", timestamp: 6756},
   ],
